b8d1fd2f807cc0abd7568e899bc506100e9c5124
DIO ANTUNES <dio.antns@gmail.com>
2024-09-22 19:06:08 -0300

    NAVBAR

--- proyecto-2/src/components/NavBar.jsx
+++ proyecto-2/src/components/NavBar.jsx
@@ -2,24 +2,31 @@ function NavBar() {
 
     return (
         <nav>
-            <div className="logo-nav">
-                <div>
-                    <img className="logo" src='./src/images/logo.svg' />
+            <div className="nav-bar">
+                <div className="menu-icon">
+                    <img src="./src/images/icon-menu.svg"/>
                 </div>
-                <ul>
-                    <li><a href='#'>Collections</a></li>
-                    <li><a href='#'>Men</a></li>
-                    <li><a href='#'>Women</a></li>
-                    <li><a href='#'>About</a></li>
-                    <li><a href='#'>Contact</a></li>
-                </ul>
+                <div className="logo">
+                    <a href="#"><img src='./src/images/logo.svg' /></a>
+                </div>
+                <div className="menu-links">
+                    <ul>
+                        <li><a href='#'>Collections</a></li>
+                        <li><a href='#'>Men</a></li>
+                        <li><a href='#'>Women</a></li>
+                        <li><a href='#'>About</a></li>
+                        <li><a href='#'>Contact</a></li>
+                    </ul>
+                </div>
             </div>
+
             <div className="cart-profile">
                 <div className="shopping-cart">
-                    <img src="./src/images/icon-cart.svg" alt="Shopping Cart"/>
+                    <a href="#"><img src="./src/images/icon-cart.svg" alt="Shopping Cart" /></a>
+                    {/* Crear función "onclick" que ejecuta componente "Shopping Cart" - nuevo componente */}
                 </div>
                 <div className="profile">
-                    <img src="./src/images/image-avatar.png" alt="Profile" />
+                    <a href="#"><img src="./src/images/image-avatar.png" alt="Profile" /></a>
                 </div>
             </div>
         </nav>
