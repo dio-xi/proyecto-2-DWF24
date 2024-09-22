3b077ee303f6782569a70219d26a34c66cdbfbf6
DIO ANTUNES <dio.antns@gmail.com>
2024-09-22 18:01:52 -0300

    NavBar

--- proyecto-2/src/components/NavBar.jsx
+++ proyecto-2/src/components/NavBar.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-
 function NavBar() {
 
     return (
         <nav>
-            <div className="logo">
-                <img src='./src/images/logo.svg'/>
-            </div>     
+            <div className="logo-nav">
+                <div>
+                    <img className="logo" src='./src/images/logo.svg' />
+                </div>
                 <ul>
                     <li><a href='#'>Collections</a></li>
                     <li><a href='#'>Men</a></li>
@@ -14,6 +13,15 @@ function NavBar() {
                     <li><a href='#'>About</a></li>
                     <li><a href='#'>Contact</a></li>
                 </ul>
+            </div>
+            <div className="cart-profile">
+                <div className="shopping-cart">
+                    <img src="./src/images/icon-cart.svg" alt="Shopping Cart"/>
+                </div>
+                <div className="profile">
+                    <img src="./src/images/image-avatar.png" alt="Profile" />
+                </div>
+            </div>
         </nav>
     )
 }
