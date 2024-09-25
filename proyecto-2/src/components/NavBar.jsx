cdef681a128ed741ce1ce9c2f039f1a7ab5a1499
DIO ANTUNES <dio.antns@gmail.com>
2024-09-25 14:07:37 -0300

    navbar finished

--- proyecto-2/src/components/NavBar.jsx
+++ proyecto-2/src/components/NavBar.jsx
@@ -1,35 +1,43 @@
+import React, { useState } from 'react';
+
 function NavBar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
-        <nav>
-            <div className="nav-bar">
-                <div className="menu-icon">
-                    <img src="./src/images/icon-menu.svg"/>
-                </div>
-                <div className="logo">
-                    <a href="#"><img src='./src/images/logo.svg' /></a>
-                </div>
-                <div className="menu-links">
-                    <ul>
-                        <li><a href='#'>Collections</a></li>
-                        <li><a href='#'>Men</a></li>
-                        <li><a href='#'>Women</a></li>
-                        <li><a href='#'>About</a></li>
-                        <li><a href='#'>Contact</a></li>
-                    </ul>
-                </div>
+        <nav className="nav-bar">
+            <div className="menu-icon" onClick={toggleMenu}>
+                <img src="./src/images/icon-menu.svg" alt="Menu Icon" />
             </div>
+            <div className="logo">
+                <a href="#"><img src='./src/images/logo.svg' alt="Logo" /></a>
+            </div>
+            <div className={`menu-links ${menuOpen ? 'open' : ''}`}>
+                <div className="close-icon" onClick={toggleMenu}>
+                    <img src="./src/images/icon-close.svg" alt="Close Icon" />
+                </div>
+                <ul>
+                    <li><a href='#'>Collections</a></li>
+                    <li><a href='#'>Men</a></li>
+                    <li><a href='#'>Women</a></li>
+                    <li><a href='#'>About</a></li>
+                    <li><a href='#'>Contact</a></li>
+                </ul>
+            </div>
+            
             <div className="cart-profile">
                 <div className="shopping-cart">
                     <a href="#"><img src="./src/images/icon-cart.svg" alt="Shopping Cart" /></a>
-                    {/* Crear función "onclick" que ejecuta componente "Shopping Cart" - nuevo componente */}
                 </div>
                 <div className="profile">
                     <a href="#"><img src="./src/images/image-avatar.png" alt="Profile" /></a>
                 </div>
             </div>
         </nav>
-    )
+    );
 }
 
-export default NavBar
+export default NavBar;
