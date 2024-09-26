f741ef1c1bb5beda88d335354439736c78ce5598
DIO ANTUNES <dio.antns@gmail.com>
2024-09-26 01:51:32 -0300

    Layout product page

--- proyecto-2/src/components/ProductPage.jsx
+++ proyecto-2/src/components/ProductPage.jsx
@@ -13,23 +13,28 @@ const handleDecrease = () => {
 return (
     <div className="product-page">
         <div className="product-details">
+            <h3>SNEAKER COMPANY</h3>
             <h1>Fall Limited Edition Sneakers</h1>
             <p>
                 These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.
             </p>
             <div className="price-section">
-                <span className="discounted-price">$125.00</span>
-                <span className="discount-label">50% OFF</span>
+                <div className="final-price">
+                    <span className="discounted-price">$125.00</span>
+                    <span className="discount-label">50% OFF</span>
+                </div>
                 <span className="original-price">$250.00</span>
             </div>
-            <div className="quantity-section">
-                <button onClick={handleDecrease}>-</button>
-                <span>{quantity}</span>
-                <button onClick={handleIncrease}>+</button>
-            </div>
-            <button className="add-to-cart">Add to cart</button>
+            <div className="quantity-cart">
+                <div className="quantity-section">
+                    <button onClick={handleDecrease}>-</button>
+                    <span>{quantity}</span>
+                    <button onClick={handleIncrease}>+</button>
+                </div>
+                <button className="add-to-cart">Add to cart</button>
             </div>
         </div>
+    </div>
     );
 };
 
--- proyecto-2/src/main.jsx
+++ proyecto-2/src/main.jsx
@@ -3,14 +3,13 @@ import { createRoot } from 'react-dom/client'
 import NavBar from './components/NavBar.jsx'
 import ProductPage from './components/ProductPage.jsx'
 import ImageGallery from './components/ImageGallery.jsx'
-import productImages from './components/productImages.js'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NavBar />
     <div className="main-content">
-      <ImageGallery images={productImages} />
+      <ImageGallery />
       <ProductPage />
     </div>
   </StrictMode>,
