9ddd988a1b1f703a38290bb8c0fcc85c6db36d63
FernandezValentina <vframos.90@gmail.com>
2024-09-29 23:21:44 -0300

    ShoppingCart window styles

--- proyecto-2/src/components/ShoppingCart.jsx
+++ proyecto-2/src/components/ShoppingCart.jsx
@@ -4,12 +4,16 @@ function ShoppingCart () {
 
     return (
         <div className="checkoutCart">
-            <p className="titleCheckout">Cart</p>
-            <img className="checkoutImage" src="./src/images/image-product-1.jpg" alt="Product" />
-            <p className="itemDescription">Fall limited edition sneakers</p>
-            <img src="./src/images/icon-delete.svg" alt="Delete" />
-            <button>Checkout</button>
-            
+            <p className="checkoutTitle">Cart</p>
+            <div class="line"></div>
+            <div className='shoppingCartGroup'>
+                <div className='imageDescription'>
+                  <img className="checkoutImage" src="./src/images/image-product-1.jpg" alt="Product" />
+                  <p className="itemDescription">Fall limited edition sneakers</p>
+                </div>
+              <img className="binImage" src="./src/images/icon-delete.svg" alt="Delete" />
+            </div>
+            <button className='shoppingCartButton'>Checkout</button>
         </div>
     )
 };
