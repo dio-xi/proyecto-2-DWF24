0ce97f1279227428daa6922e9fce8e09787eefc3
DIO ANTUNES <dio.antns@gmail.com>
2024-10-01 21:05:07 -0300

    Shopping cart css updates

--- proyecto-2/src/components/ShoppingCart.jsx
+++ proyecto-2/src/components/ShoppingCart.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import './ShoppingCart.css';
 
 function ShoppingCart({ cartItems, setCartItems }) {
@@ -12,31 +12,34 @@ function ShoppingCart({ cartItems, setCartItems }) {
         <section className="checkoutCart">
             <h2 className="checkoutTitle">Cart</h2>
             <hr className="line" />
+            <div className="shoppingCart">
+                {cartItems.length > 0 ? (
+                    <>
+                        {cartItems.map(item => (
+                            <article key={item.id} className='shoppingCartGroup'>
+                                <div className='imageDescription'>
+                                    <img className="checkoutImage" src={item.image} alt={item.name} />
+                                    <div className='itemDescription'>
+                                        <p className="itemName">{item.name}</p>
+                                        <p className="itemQuantity">${item.price.toFixed(2)} x {item.quantity} <strong>${(item.price * item.quantity).toFixed(2)}</strong></p>
+                                    </div>
 
-            {cartItems.length > 0 ? (
-                <>
-                    {cartItems.map(item => (
-                        <article key={item.id} className='shoppingCartGroup'>
-                            <div className='imageDescription'>
-                                <img className="checkoutImage" src={item.image} alt={item.name} />
-                                <p className="itemDescription">{item.name}</p>
-                                
-                                <p>${item.price.toFixed(2)} x {item.quantity} <strong>${(item.price * item.quantity).toFixed(2)}</strong></p>
-                            </div>
-                            <button
-                                className="binButton"
-                                aria-label="Remove item"
-                                onClick={() => removeItem(item.id)}
-                            >
-                                <img className="binImage" src="./src/images/icon-delete.svg" alt="Delete" />
-                            </button>
-                        </article>
-                    ))}
-                    <button className='shoppingCartButton'>Checkout</button>
-                </>
-            ) : (
-                <p className='emptyCart'>Your cart is empty.</p>
-            )}
+                                </div>
+                                <button
+                                    className="binButton"
+                                    aria-label="Remove item"
+                                    onClick={() => removeItem(item.id)}
+                                >
+                                    <img className="binImage" src="./src/images/icon-delete.svg" alt="Delete" />
+                                </button>
+                            </article>
+                        ))}
+                        <button className='shoppingCartButton'>Checkout</button>
+                    </>
+                ) : (
+                    <p className='emptyCart'>Your cart is empty.</p>
+                )}
+                </div>
         </section>
     );
 }
