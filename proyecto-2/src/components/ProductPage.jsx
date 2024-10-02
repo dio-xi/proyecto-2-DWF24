067f372668ab061e2843a2965c4f57e9b23e1d9b
FernandezValentina <vframos.90@gmail.com>
2024-10-01 23:57:49 -0300

    Add cart icon to button and modify text color

--- proyecto-2/src/components/ProductPage.jsx
+++ proyecto-2/src/components/ProductPage.jsx
@@ -80,6 +80,7 @@ const ProductPage = ({ cartItems, setCartItems }) => {
                         <button onClick={handleIncrease}>+</button>
                     </div>
                     <button className="add-to-cart" onClick={handleAddToCart}>
+                    <img src= "src/images/icon-cart.svg" alt="Cart Icon" className="cart-icon" />
                         Add to cart
                     </button>
                 </div>
